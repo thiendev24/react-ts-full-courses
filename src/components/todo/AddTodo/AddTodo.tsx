c10ec66e60cee67f1b89e2877154050c628ceb27
Minh Thien <thien.nhm92@gmail.com>
2023-05-22 17:32:51 +0700

    useRef with Form

--- src/components/todo/AddTodo/AddTodo.tsx
+++ src/components/todo/AddTodo/AddTodo.tsx
@@ -1,64 +1,54 @@
-import { useState } from "react";
-import { Button, Container } from "react-bootstrap";
+import { useRef } from "react";
+import { Button, Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { todos } from "../../../database/todoList";
 import { v4 as uuidv4 } from "uuid";
 
 const AddTodo = () => {
-  const [todo, setTodo] = useState<string>("");
+  const todoRef = useRef<HTMLInputElement | null>(null);
 
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTodo(e.target.value);
-    console.log(todo);
-  };
+  const handleCreateTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(todoRef);
 
-  const handleCreateTodo = () => {
-    if (todo) {
+    if (todoRef.current?.value) {
       const newTodo = {
         id: uuidv4(),
-        title: todo,
+        title: todoRef.current?.value,
         time: new Date().toISOString(),
       };
-      todos.push(newTodo);
+      console.log(newTodo);
     } else {
       alert(`Can't empty toto field!`);
     }
   };
+
   return (
     <Container fluid>
       <h3 className="text-center text-danger mt-2">Add Todo</h3>
       <div>
-        <form>
-          <div className="mb-3">
-            <label htmlFor="title" className="form-label">
-              Title
-            </label>
-            <input
+        <Form onSubmit={(e) => handleCreateTodo(e)}>
+          <Form.Group controlId="title" className="mb-3">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
               type="text"
-              className="form-control"
-              id="title"
               placeholder="Enter new todo"
-              onChange={(e) => onChangeInput(e)}
+              ref={todoRef}
             />
-          </div>
+          </Form.Group>
           <div className="mt-3 float-end">
-            <Link to="/todos">
-              <Button
-                type="submit"
-                variant="primary"
-                className="me-2"
-                onClick={() => handleCreateTodo()}
-              >
-                Create
-              </Button>
-            </Link>
+            {/* <Link to="/todos"> */}
+            <Button type="submit" variant="primary" className="me-2">
+              Create
+            </Button>
+            {/* </Link> */}
             <Link to="/todos">
               <Button type="button" variant="outline-secondary">
                 Cancel
               </Button>
             </Link>
           </div>
-        </form>
+        </Form>
       </div>
     </Container>
   );
